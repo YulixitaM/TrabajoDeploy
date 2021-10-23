b931a96b58bdc98a67762d3b058d243b96eedc10
Yuli Martinez <yulimartinez@unicomfacauca.edu.co>
2021-10-22 20:12:31 -0500

    Mi primer repositorio

--- app.py
+++ app.py
@@ -18,29 +18,22 @@ def prediccion():
     xin = json['Datos']
     xin = np.asarray(xin)
     xin = xin.reshape(1,13)
-    # print("Servicio post")
+   # print("Servicio post")
     yout = model.predict(xin)
     print(yout)
     mensaje = ''
     for y_out in yout:
-        mensaje = mensaje + 'El paciente ' + labels[y_out] + ' tiene una enfermedad cardiaca\n'
+        mensaje = mensaje + 'El paciente se encuentra ' + labels[y_out]
     
     return mensaje
 
-@app.route('/saludo',methods=['POST'])
-def saludo():
-    json = request.get_json(force=True)
-    nombre = json['nombre']
-    apellido = json['apellido']
-    return 'Hola ' + nombre + ' ' + apellido
-
 
 # Cargar modelo
-pkl_filename = 'RF_heart_v2.pkl'
+pkl_filename = 'Heart_model.pkl'
 with open(pkl_filename, 'rb') as file:
     model = pickle.load(file)
     
-labels = ['No', 'SI'] # Etiquetas de datos
+labels = ['SANO', 'ENFERMO'] # Etiquetas de datos
 
 if __name__ == '__main__':
     app.run()
